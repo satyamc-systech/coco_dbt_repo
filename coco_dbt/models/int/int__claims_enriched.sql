6bca19aa50b431da2a191a3128bf1be19f36e501
satyamc-systech <satyamc@systechusa.com>
2026-03-22 11:20:20 +0000

    added new int model

---
+++ coco_dbt/models/int/int__claims_enriched.sql
@@ -0,0 +1,53 @@
+{{ config(
+    materialized='table'
+) }}
+
+with stg_claims as (
+
+    select
+        claim_id,
+        claim_number,
+        policy_id,
+        claim_status,
+        claim_type,
+        incident_date,
+        report_date,
+        claim_amount,
+        approved_amount,
+        deductible_applied,
+        incident_description,
+        adjuster_name,
+        settlement_date,
+        created_at,
+        updated_at
+    from {{ ref('stg__insurance_raw__claims') }}
+
+),
+
+final__claims_enriched as (
+
+    select
+        claim_id,
+        claim_number,
+        policy_id,
+        claim_status,
+        claim_type,
+        incident_date,
+        report_date,
+        settlement_date,
+        claim_amount,
+        approved_amount,
+        deductible_applied,
+        approved_amount - deductible_applied as net_payout_amount,
+        claim_amount - approved_amount as denied_amount,
+        datediff('day', incident_date, report_date) as days_to_report,
+        datediff('day', report_date, settlement_date) as days_to_settle,
+        incident_description,
+        adjuster_name,
+        created_at,
+        updated_at
+    from stg_claims
+
+)
+
+select * from final__claims_enriched
